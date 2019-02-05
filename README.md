ea2f1bde8e98c9c08fdde8fc78d05539caacc8d8
Brennan Fairfull <brennan.fairfull@gmail.com>
2019-02-05 19:55:44 +1100

    add video player component

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Angular 6 By Example
 Source control repository for upcoming Angular 6 By Example book.
 
-# UPTO document page #63
+# UPTO document page #76
--- trainer/src/app/workout-runner/workout-runner.module.ts
+++ trainer/src/app/workout-runner/workout-runner.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WorkoutRunnerComponent } from './workout-runner.component';
 import { ExerciseDescriptionComponent } from './exercise-description/exercise-description.component';
+import { VideoPlayerComponent } from './video-player/video-player.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [WorkoutRunnerComponent, ExerciseDescriptionComponent],
-  exports:[WorkoutRunnerComponent, ExerciseDescriptionComponent]
+  declarations: [WorkoutRunnerComponent, ExerciseDescriptionComponent, VideoPlayerComponent],
+  exports:[WorkoutRunnerComponent]
 })
 export class WorkoutRunnerModule { }
